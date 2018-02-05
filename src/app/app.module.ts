cf954ed3e254ad1a47053f9087b12c6a66303f3a
Axel Olsson <olsson.axel@gmail.com>
2018-02-05 20:31:01 +0100

    Add angular material, sidebar and some routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
+
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
+import { NotFoundComponent } from './notfound/notfound.component';
+import { MatCommonModule } from '@angular/material/core/typings/common-behaviors/common-module';
+
+const routes: Routes = [
+  { path : '', component: RecipesComponent },
+  { path : 'recipe/:id', component: RecipesComponent },
+  { path : 'saved/:id', component: RecipeListComponent },
+  { path : '**', component: NotFoundComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     RecipesComponent,
     RecipeListComponent,
-    RecipeDetailsComponent
+    RecipeDetailsComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    BrowserModule,
+    RouterModule.forRoot(
+      routes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
